d4fbd8665b4e4452466cc3a1242b045f60c0cb42
navidmirzad <navi0221@stud.kea.dk>
2022-12-13 22:44:29 +0100

    gettere & settere

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+    Person person1 = new Person("Navid", "Mirzad");
+        System.out.println(person1);
+
+
 
     }
 
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -16,6 +16,30 @@ public class Person {
                 "\nPoints: " + points;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+   public void setFirstName(String firstName) {
+        this.firstName = firstName;
+   }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
 
 
 }
