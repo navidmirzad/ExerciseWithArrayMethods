177315dd70d1ff52506facff1a2bde8b6fd2bba7
navidmirzad <navi0221@stud.kea.dk>
2022-12-14 00:22:48 +0100

    system.out.print array personer with enhanced for loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,19 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-    Person person1 = new Person("Harry", "Potter");
-    Person person2 = new Person("Ron", "Weasley");
-    Person person3 = new Person("Hermione", "Granger");
+        Person person1 = new Person("Harry", "Potter");
+        Person person2 = new Person("Ron", "Weasley");
+        Person person3 = new Person("Hermione", "Granger");
 
-    Person personer[] = {person1, person2, person3};
+        Person personer[] = {person1, person2, person3};
 
-        System.out.println(Arrays.toString(personer));
+        for (Person person : personer) {
+            System.out.println(person);
+        }
 
-
+        // System.out.println(Arrays.toString(personer));
 
         //System.out.println(person1 + "\n" + person2 + "\n" + person3);
-
 
 
     }
